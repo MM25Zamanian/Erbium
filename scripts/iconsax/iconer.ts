de0666c18d0ff45b7a0954e412e02d7daa5742c5
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-05-09 20:37:33 +0430

    feat(scripts): iconsax scripts

---
+++ scripts/iconsax/iconer.ts
@@ -0,0 +1,43 @@
+import {readdir, writeFile} from 'fs/promises';
+
+const iconsPath = '../../assets/iconsax-svg';
+
+let list: string[] = [];
+
+const start = async (): Promise<string[]> => {
+  list = [];
+  const iconsCategories = (await readdir(iconsPath)).filter((cat) => !cat.includes('.', 0));
+  for await (const category of iconsCategories) {
+    if (category === 'Crypto') {
+      const cryptoIconsCategories = (await readdir(iconsPath + '/' + category)).filter(
+          (cat) => !cat.includes('.', 0),
+      );
+      cryptoIconsCategories.forEach(async (cryptoIconsCategorie) => {
+        const icons = (
+          await readdir(iconsPath + '/' + category + '/' + cryptoIconsCategorie)
+        ).filter((icon) => icon.includes('.svg'));
+
+        for (const icon of icons) {
+          list.push(icon);
+          // console.log(`${cryptoIconsCategorie}: ${icon} - ${category}`);
+        }
+      });
+    } else {
+      const icons = (await readdir(iconsPath + '/' + category)).filter((icon) =>
+        icon.includes('.svg'),
+      );
+
+      for (const icon of icons) {
+        list.push(icon);
+        // console.log(`${category}: ${icon}`);
+      }
+    }
+  }
+  console.log(list);
+  list = list.filter((item, index) => list.includes(item, index));
+  console.log(list);
+  writeFile('./list.txt', list.join('\n'));
+  return list;
+};
+
+start();
